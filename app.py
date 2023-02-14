0fd4c0ed864f660f4e0a80af7c09aaf63bb6805e
ffernandezz07 <125408607+ffernandezz07@users.noreply.github.com>
2023-02-14 14:09:34 -0800

    b

--- app.py
+++ app.py
@@ -11,10 +11,17 @@ df = pd.read_csv(ticker + '.csv', parse_dates=['Date'], index_col=['Date'])
 begDate = df.index.min()
 endDate = df.index.max()
 
-pickStart = st.date_input("Pick start date:", begDate)
+pickStart = st.date_input("Pick start date:", begDate, min_value=begDate)
 pickEnd = st.date_input("Pick end date:", endDate)
 
+filterDF = df.loc[pickStart:pickEnd] #keep the filter version of table as new variable
+st.write(filterDF)
 
-st.write(df.loc[pickStart:pickEnd])
+#P3 Lessons floder - copy codes to create chaers
+import plotly.express as px
+fig = px.line(filterDF.index, y= "Close")
+st.plotly_chart(fig)
+
+
 st.write(begDate)
 st.write(endDate)
