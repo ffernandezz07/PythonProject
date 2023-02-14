47f9d22bb78f2f323df1c5d905a87c77a3e0d43c
ffernandezz07 <125408607+ffernandezz07@users.noreply.github.com>
2023-02-14 15:51:37 -0800

    a

--- assigment2.py
+++ assigment2.py
@@ -2,5 +2,5 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
-df = pd.read_excel("Data Manipulation Worksheet.xlsx")
-st.write(df)
+df = pd.read_excel("ExData\Data Manipulation Worksheet.xlsx", sheet_name ="Financing Table Clean", parse_dates=['DATE'])
+df.set_index(['DATE'], inplace=True)
