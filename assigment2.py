bc6c99d9cc64bbd8bdfb2a87b2e9ae916cdc38dd
ffernandezz07 <125408607+ffernandezz07@users.noreply.github.com>
2023-02-14 16:19:46 -0800

    a

--- assigment2.py
+++ assigment2.py
@@ -10,11 +10,13 @@ st.title('Financial Deals Data through Plotly Graphs')
 chart_type = ["Bar", "Box & Whisker", "Sunburst","Three Map"]
 x_names = list(df.columns.values) 
 legend_options = list(df.columns.values) 
-
+hover = list(df.columns.values)
 
 
 chart_type = st.sidebar.radio("Pick a chart type", options=chart_type)
 x_ax = st.sidebar.selectbox("Pick a category for the x-axis", options=x_names)
 legend = st.sidebar.selectbox("Pick a category for the legend filter", options=legend_options)
+hover = st.sidebar.selectbox("Pick a category for the hover", options=legend_options)
+
 
 st.write('Total Deal Value by ' + x_ax + ' by ' + legend)
