dbe5e6af3c27a33472656cd14f6abaf3739f081c
ffernandezz07 <125408607+ffernandezz07@users.noreply.github.com>
2023-02-14 16:12:26 -0800

    a

--- assigment2.py
+++ assigment2.py
@@ -6,11 +6,14 @@ df = pd.read_excel("Data Manipulation Worksheet.xlsx", sheet_name ="Financing Ta
 df.set_index(['DATE'], inplace=True)
 
 st.title('Financial Deals Data through Plotly Graphs')
-st.write('Total Deal Value by Type by Industry')
+st.write('Total Deal Value by  by Industry')
 
 chart_type = ["Bar", "Box & Whisker", "Sunburst","Three Map"]
 x_names = list(df.columns.values) 
+legend_options = list(df.columns.values) 
 
 
-st.sidebar.radio("Pick a chart type", options=chart_type)
+
+chart_type = st.sidebar.radio("Pick a chart type", options=chart_type)
 st.sidebar.selectbox("Pick a category for the x-axis", options=x_names)
+st.sidebar.selectbox("Pick a category for the legend filter", options=legend_options)
